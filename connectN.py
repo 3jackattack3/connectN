941e21f1cfb673ad082e82946fbb0c19c2d1ffaf
3jackattack3 <3jackattack3@gmail.com>
2018-01-12 23:45:21 -0500

    working out checker function

--- connectN.py
+++ connectN.py
@@ -10,57 +10,58 @@ def displayBoard(board):
 def placePiece(board, column, color):
     location = 0
     for i in range(len(board)):
-        if(board[i][column] == '.'):
+        if board[i][column] == '.':
             location += 1
     location -= 1
 
-    if(color == "red"):
+    if color == "red":
         board[location][column] = 'X'
     else:
         board[location][column]= 'O'
 
-def checkVerticals(board):
-    counter = 0
-    winRed = False
-    winBlack = False
-    for column in range(len(board)):
-        for i in range(int((len(board)-1)/2)):
-            for j in range(int((len(board)+1)/2)):
-                if(board[j][column] == board[j+1][column]):
-                    counter += 1
-                if(counter >= (len(board)+1)/2 and board[j][column] == 'X'):
-                    winRed = True
-                if(counter >= (len(board)+1)/2 and board[j][column] == 'O'):
-                    winBlack = True
-                counter = 0
-    return [winRed, winBlack]
+# def checkVerticals(board):
+#     counter = 0
+#     winRed = False
+#     winBlack = False
+#     for column in range(len(board)):
+#         for i in range(int((len(board)-1)/2)):
+#             for j in range(int((len(board)+1)/2)):
+#                 if(board[j][column] == board[j+1][column]):
+#                     counter += 1
+#                 if(counter >= (len(board)+1)/2 and board[j][column] == 'X'):
+#                     winRed = True
+#                 if(counter >= (len(board)+1)/2 and board[j][column] == 'O'):
+#                     winBlack = True
+#                 counter = 0
+#     return [winRed, winBlack]
 
 def check(board):
     winRed = False
     winBlack = False
-    for i in range(startx, len(board)):
-        for j in range(starty, len(board[column])):
-            if(board[j][column] == 'X'):
-                winRed = check(board, i, j, red)
-            if(board[j][column] == 'O'):
-                winBlack = check(board, i, j, black)
+    for column in board:
+        for j in range(len(board)):
+            if column[j] == 'X':
+                winRed = checkSameDirection(board, board.index(column), j, "red")
+            if column[j] == 'O':
+                winBlack = checkSameDirection(board, board.index(column), j, "black")
     return [winRed, winBlack]
 
 def checkSameDirection(board, startx, starty, color):
+    print("in function")
     win = False
-    if(color == "red"):
+    if color == "red":
         counterRed = 1
-    elif(color == "black"):
+    elif color == "black":
         counterBlack = 1
 
     for i in range(startx, len(board)):
         for j in range(starty, len(board[column])):
-            if(color == "red"):
+            if color == "red":
                 counterRed += 1
-            elif(color == "black"):
+            elif color == "black":
                 counterBlack += 1
 
-    if(counterRed >= (len(board)+1)/2 or counterBlack >= (len(board)+1)/2):
+    if counterRed >= (len(board)+1)/2 or counterBlack >= (len(board)+1)/2:
         win = True
 
     return win
@@ -87,6 +88,6 @@ placePiece(board, 3, "red")
 placePiece(board, 3, "red")
 placePiece(board, 3, "red")
 placePiece(board, 3, "red")
-win = checkVerticals(board)
+win = check(board)
 displayBoard(board)
 print(win)
